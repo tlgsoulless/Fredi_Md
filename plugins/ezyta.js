df25edc4f0bfd50e22ad24e87957abaa8bce512a
FredieTech <frediezra60@gmail.com>
2025-01-14 12:41:50 +0300

    Add files via upload

---
+++ plugins/ezyta.js
@@ -0,0 +1,50 @@
+const config = require('../config');
+const { cmd } = require('../command');
+const { ytsearch, ytmp3, ytmp4 } = require('@dark-yasiya/yt-dl.js'); 
+
+
+cmd({
+    pattern: "play3",
+    alias: ["audio2","song2","ytsong"],
+    react: "🎶",
+    desc: "Download Youtube song",
+    category: "main",
+    use: '.song < Yt url or Name >',
+    filename: __filename
+},
+async(conn, mek, m,{ from, prefix, quoted, q, reply }) => {
+try{
+
+if(!q) return await reply("Please give me Yt url or Name")
+	
+const yt = await ytsearch(q);
+if(yt.results.length < 1) return reply("Results is not found !")
+
+let yts = yt.results[0]  
+const ytdl = await ytmp3(yts.url)
+		
+let ytmsg = `╭━━━〔 *✧FREDI MD✧* 〕━━━┈⊷
+┃▸┃๏ *MUSIC DOWNLOADER*
+╭━━❐━⪼
+┇๏ *Tital* -  ${yts.title}
+┇๏ *Duration* - ${yts.timestamp}
+┇๏ *Views* -  ${yts.views}
+┇๏ *Author* -  ${yts.author.name} 
+┇๏ *Link* -  ${yts.url}
+╰━━❑━⪼
+> *© regards frediezra ♡*`
+// SEND DETAILS
+await conn.sendMessage(from, { image: { url: yts.thumbnail || yts.image || '' }, caption: `${ytmsg}`}, { quoted: mek });
+
+// SEND AUDIO TYPE
+await conn.sendMessage(from, { audio: { url: ytdl.download.url }, mimetype: "audio/mpeg" }, { quoted: mek })
+
+// SEND DOC TYPE
+await conn.sendMessage(from, { document: { url: ytdl.download.url }, mimetype: "audio/mpeg", fileName: ytdl.result.title + '.mp3', caption: `> *© Pᴏᴡᴇʀᴇᴅ Bʏ ✧FREDI MD✧*` }, { quoted: mek })
+
+
+} catch (e) {
+console.log(e)
+reply(e)
+}}
+)
